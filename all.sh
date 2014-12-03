b3491106079d6784c68a98c65ab491c946a40b4e
calaba <calaba@gmail.com>
2014-12-03 13:43:57 -0800

    Version 0.17

--- all.sh
+++ all.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+echo "Installing required development tools & libraries ..."
 ./1-compile-install-prereq.sh &> 1-compile-install-prereq.log
 
+echo "Unpacking source codes ..."
 ./2-compile-unpack-src.sh &> 2-compile-unpack-src.log
 
+echo "Compiling required libraries with 64-bit indexing support ..."
 TIME="\nExecution Time of Libraries Compilation: %E \n"
 time ./3-compile-64-libs.sh &> 3-compile-64-libs.log
 
+echo "Compiling Octave source code with 64-bit indexing support..."
 TIME="\nExecution Time of Octave Compilation: %E \n"
 time ./4-compile-64-octave.sh &> 4-compile-64-octave.log
 
+echo "Octave compilation & internal tests finished, press a key to run octave ..."
+pause
+ 
 ./5-run_64bit_octave.sh 
+
+echo "Octave interactive test done, if you are satisfied run 'sudo make install' from ${octave64src} directory ..."
