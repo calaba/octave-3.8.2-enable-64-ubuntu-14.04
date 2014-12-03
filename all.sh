9e11610d9e01456b246ed02b4a30184a6db41504
ric <calaba@gmail.com>
2014-12-02 22:18:45 -0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-./1-compile-install-prereq.sh >& 1-compile-install-prereq.log
-./2-compile-unpack-src.sh >& 2-compile-unpack-src.log
-./3-compile-64-libs.sh >& 3-compile-64-libs.log
-./4-compile-64-octave.sh >& 4-compile-64-octave.log
+
+./1-compile-install-prereq.sh &> 1-compile-install-prereq.log
+
+./2-compile-unpack-src.sh &> 2-compile-unpack-src.log
+
+TIME="\nExecution Time of Libraries Compilation: %E \n"
+time ./3-compile-64-libs.sh &> 3-compile-64-libs.log
+
+TIME="\nExecution Time of Octave Compilation: %E \n"
+time ./4-compile-64-octave.sh &> 4-compile-64-octave.log
+
 ./5-run_64bit_octave.sh 
