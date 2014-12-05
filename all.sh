76d8a801d8de29cc7656162e3c353963592aed8f
ric <calaba@gmail.com>
2014-12-04 22:56:30 -0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -3,33 +3,28 @@
 my_dir="$(dirname "$0")"
 . "$my_dir/compile-params.in"
 
-echo "Installing required development tools & libraries ..."
+echo "`date` : Installing required development tools & libraries ..."
 ./1-compile-install-prereq.sh > 1-compile-install-prereq.log 2>&1
 
 cd ${octave64_gitroot}
-echo "Unpacking source codes ..."
+echo "`date` : Unpacking source codes ..."
 ./2-compile-unpack-src.sh > 2-compile-unpack-src.log 2>&1
 
 cd ${octave64_gitroot}
-echo "Compiling required libraries with 64-bit indexing support ..."
-TIME="\nExecution Time of Libraries Compilation: %E \n"
-time ./3-compile-64-libs.sh > 3-compile-64-libs.log 2>&1
+echo "`date` : Compiling required libraries with 64-bit indexing support ..."
+./3-compile-64-libs.sh > 3-compile-64-libs.log 2>&1
 
 cd ${octave64_gitroot}
-echo "Compiling Octave source code with 64-bit indexing support..."
-TIME="\nExecution Time of Octave Compilation: %E \n"
-time ./4-compile-64-octave.sh > 4-compile-64-octave.log 2>&1
+echo "`date` : Compiling Octave source code with 64-bit indexing support..."
+./4-compile-64-octave.sh > 4-compile-64-octave.log 2>&1
 
 cd ${octave64_gitroot}
-echo "Octave compilation and internal tests finished, press a key to run octave ..."
+echo "`date` : Octave compilation and internal tests finished, press a key to run octave ..."
 read  -p ":" keyinput -n 1
 
 ./5-run_64bit_octave.sh 
 
 echo
-echo "Octave interactive test done."
+echo "`date` : Octave interactive test done."
 echo "To install octave run: 'sudo make install' from ${octave64src} ..."
 echo
-
-
-
