5c77e7c73548df41141a70a8830c3d6cfeb9a56a
ric <calaba@gmail.com>
2014-12-04 22:22:10 -0800

    Update 4-compile-64-octave.sh

--- 4-compile-64-octave.sh
+++ 4-compile-64-octave.sh
@@ -13,9 +13,8 @@ cp -Rf ${libs64src_mods}/octave-src/* ./
 
 # ./configure CPPFLAGS="-I$SUITESPARSE/include -I$QHULL/include " LIBS="-L$SUITESPARSE/lib -lmetis -L$QHULL/lib -L$ARPACK/lib -larpack -L$LAPACK/lib -L$BLAS/lib -lblas -lgfortran -lm -lpthread" FFLAGS=-fdefault-integer-8 F77=gfortran --enable-64
 # ./configure LIBS=${libs64} LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib" --enable-64
-# ./configure LIBS=${libs64} LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib" --enable-64 --disable-docs --enable-shared --enable-dl
 
-./configure ${octave64_config_extra} --enable-64 LIBS="${libs64}" LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib"
+./configure ${octave64_config_extra} --prefix=${prefix64} --enable-64 LIBS="${libs64}" LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib"
 
 # build 
 make clean
