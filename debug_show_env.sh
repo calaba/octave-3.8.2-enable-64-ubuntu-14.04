7e4d34f743d5dcda0a1b664b07661ed8b51611ab
calaba <calaba@gmail.com>
2014-12-05 18:09:50 -0800

    Version 0.69

---
+++ debug_show_env.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo ======== ENVIRONMENT VARIABLES FOR 'Octave with 64-bit indexing' COMPILATION 
+echo 
+echo "octave64_gitroot:         ${octave64_gitroot}"
+echo "prefix64:                 ${prefix64}"
+echo "octave64_compilation_test:${octave64_compilation_test}"
+echo "octave64_config_extra:    ${octave64_config_extra}"
+echo "JAVA_HOME:                ${JAVA_HOME}"
+echo "octave64src:              ${octave64src}"
+echo "libs64src_archives:       ${libs64src_archives}"
+echo "libs64src_mods:           ${libs64src_mods}"
+echo "libs64src:                ${libs64src}"
+echo "libs64:                   ${libs64}"
+echo "LD_LIBRARY_PATH:          ${LD_LIBRARY_PATH}"
+echo "LD_RUN_PATH:              ${LD_RUN_PATH}"
+echo "SUITESPARSE:              ${SUITESPARSE}"
+echo "QHULL:                    ${QHULL}"
+echo "ARPACK:                   ${ARPACK}"
+echo "BLAS:                     ${BLAS}"
+echo "LAPACK:                   ${LAPACK}"
+echo ======== ENVIRONMENT VARIABLES =======================================
