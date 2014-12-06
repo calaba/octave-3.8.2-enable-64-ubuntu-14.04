b01e0893da129914990a296bd37d54ba08ad87f1
ric <calaba@gmail.com>
2014-12-05 18:18:05 -0800

    Update 4-compile-64-octave.sh

--- 4-compile-64-octave.sh
+++ 4-compile-64-octave.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 
+##########################################################################################
+### Initialize environment
+##########################################################################################
+
 # include main compilation parameters
 my_dir="$(dirname "$0")"
 . "$my_dir/compile-params.in"
 
-# compile Octave src - 1st configure, then make and then make check
-
 cd ${octave64src}
+
+##########################################################################################
+### Compile Octave with 64-bit Indexing Enabled
+##########################################################################################
 
 # copy modification of octave source code
 cp -Rf ${libs64src_mods}/octave-src/* ./
 
-# ./configure CPPFLAGS="-I$SUITESPARSE/include -I$QHULL/include " LIBS="-L$SUITESPARSE/lib -lmetis -L$QHULL/lib -L$ARPACK/lib -larpack -L$LAPACK/lib -L$BLAS/lib -lblas -lgfortran -lm -lpthread" FFLAGS=-fdefault-integer-8 F77=gfortran --enable-64
-# ./configure LIBS=${libs64} LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib" --enable-64
-
+# configure compilation
 ./configure ${octave64_config_extra} --prefix=${prefix64} --enable-64 LIBS="${libs64}" LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib"
 
 # build 
 make clean
 make
 
-# test compiled sources by executing internal chcecks
-make check
+
+##########################################################################################
+### Test Octave
+##########################################################################################
+
+# test compilation if requested
+if [ "${octave64_compilation_test}" = "Y" ] ; then 
+# test compiled sources by executing internal checks
+  make check
+fi
