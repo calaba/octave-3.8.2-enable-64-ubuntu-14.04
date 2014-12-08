1978c7c61c2bb4ee267a40d8a2b9430bc9f0fcb3
ric <calaba@gmail.com>
2014-12-07 16:45:01 -0800

    Update make_so_lib.sh

--- x64-libs/_mods/ARPACK.mod/make_so_lib.sh
+++ x64-libs/_mods/ARPACK.mod/make_so_lib.sh
@@ -1,6 +1,7 @@
- mkdir tmp 
- cd tmp 
- ar x ../libarpack.a 
- gcc -shared -o ../libarpack.so *.o -L${prefix64}/lib -llapack -lblas 
- cd .. 
- rm -Rf tmp 
+#!/bin/sh
+mkdir tmp 
+cd tmp 
+ar x $1/lib/libarpack.a 
+gcc -shared -o $1/lib/libarpack.so *.o -L$1/lib -llapack -lblas 
+cd .. 
+rm -Rf tmp 
