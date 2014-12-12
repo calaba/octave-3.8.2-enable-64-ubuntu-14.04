baea9817f626a9a7fd4df662ca758d2e66652bf9
ric <calaba@gmail.com>
2014-12-11 22:35:23 -0800

    Update 4-compile-64-octave.sh

--- 4-compile-64-octave.sh
+++ 4-compile-64-octave.sh
@@ -17,34 +17,36 @@ cd ${octave64src}
 # copy modification of octave source code
 cp -Rf ${libs64src_mods}/octave-src/* ./
 
-# configure compilation
+# configure compilation - depends on the SuiteSparse library version
 
-# getting compilation error if cholmod is found and used (seems colamd and ccolamd also use cholmod ... same error)
-# ./configure \
-# --with-colamd="-lcolamd -lsuitesparseconfig -lrt" \
-# --with-colamd-includedir=${prefix64}/include --with-colamd-libdir=${prefix64}/lib \
-# --with-ccolamd="-lccolamd -lsuitesparseconfig -lrt" \
-# --with-ccolamd-includedir=${prefix64}/include --with-ccolamd-libdir=${prefix64}/lib \
-# --with-cholmod="-lcholmod -lsuitesparseconfig -lrt" \
-# --with-cholmod-includedir=${prefix64}/include --with-cholmod-libdir=${prefix64}/lib \
-# --with-umfpack="-lumfpack -lsuitesparseconfig -lrt" \
-# --with-umfpack-includedir=${prefix64}/include --with-umfpack-libdir=${prefix64}/lib \
-# ${octave64_config_extra} --prefix=${prefix64} --enable-64 LIBS="${libs64}" LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib"
-
+case ${octave64_SS_version} in
 # This works - fine - only 3 WARNINGS in configure (colamd, ccolamd, cholmod not found) 
 # - compilation ok and tests with results PASS 11543, FAIL 0, XFAIL 7, SKIPPED 54
-./configure \
---with-colamd-includedir=${prefix64}/include --with-colamd-libdir=${prefix64}/lib \
---with-ccolamd-includedir=${prefix64}/include --with-ccolamd-libdir=${prefix64}/lib \
---with-cholmod-includedir=${prefix64}/include --with-cholmod-libdir=${prefix64}/lib \
---with-umfpack="-lumfpack -lsuitesparseconfig -lrt" \
---with-umfpack-includedir=${prefix64}/include --with-umfpack-libdir=${prefix64}/lib \
-${octave64_config_extra} --prefix=${prefix64} --enable-64 LIBS="${libs64}" LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib"
+    4.2.1 ) ./configure \
+              --with-colamd="-lcolamd -lsuitesparseconfig -lrt" \
+              --with-colamd-includedir=${prefix64}/include --with-colamd-libdir=${prefix64}/lib \
+              --with-ccolamd="-lccolamd -lsuitesparseconfig -lrt" \
+              --with-ccolamd-includedir=${prefix64}/include --with-ccolamd-libdir=${prefix64}/lib \
+              --with-cholmod="-lcholmod -lsuitesparseconfig -lrt" \
+              --with-cholmod-includedir=${prefix64}/include --with-cholmod-libdir=${prefix64}/lib \
+              --with-umfpack="-lumfpack -lsuitesparseconfig -lrt" \
+              --with-umfpack-includedir=${prefix64}/include --with-umfpack-libdir=${prefix64}/lib \
+              ${octave64_config_extra} --prefix=${prefix64} --enable-64 LIBS="${libs64}" LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib" ;;
+# getting compilation error if cholmod is specified (seems colamd and ccolamd also use cholmod ... same error)
+# thus we will compile without cholmod, colamd, ccolamd
+    4.4.2 ) ./configure \
+              --without-colamd \
+              --without-ccolamd \
+              --without-cholmod \ 
+              --with-umfpack="-lumfpack -lsuitesparseconfig -lrt" \
+              --with-umfpack-includedir=${prefix64}/include --with-umfpack-libdir=${prefix64}/lib \
+              ${octave64_config_extra} --prefix=${prefix64} --enable-64 LIBS="${libs64}" LD_LIBRARY_PATH="${prefix64}/lib" CPPFLAGS="-I${prefix64}/include" LDFLAGS="-L${prefix64}/lib" ;;
+    * ) echo "SuiteSparse version ${octave64_SS_version} - compilation not tested/supported ... "; exit 1 ;;
+esac
 
 # build 
 make clean
 make
-
 
 ##########################################################################################
 ### Test Octave
